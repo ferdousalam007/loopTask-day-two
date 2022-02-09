4f1b11b043eaa13f89d98054343b0796bef9e5bb
ferdousalam007 <iamferdousalam007@gmail.com>
2022-02-09 11:27:14 +0600

    first commit

---
+++ loopTask.js
@@ -0,0 +1,19 @@
+console.log("example one");
+for (let i = 1; i < 10; i++) {
+    let line = "*";
+    for (let j = 1; j < i; j++) {
+
+        line += "*";
+    }
+    console.log(line)
+}
+console.log("----------------------------");
+console.log("example two");
+let totalLine = 10;
+for (let a = 0; a < totalLine; a++) {
+    let lineData = "";
+    for (let b = 0; b < totalLine; b++) {
+        lineData += "*"
+    }
+    console.log(lineData);
+}
